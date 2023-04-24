591334fe2455a76d8aa65f9619bc2af5068fee7a
Sahana20212 <96504836+Sahana20212@users.noreply.github.com>
2023-04-24 07:58:05 -0500

    Classcreated.

---
+++ src/com/core/Addition.java
@@ -0,0 +1,13 @@
+package com.core;
+
+public class Addition {//This is the class declaration.
+	
+	int a=5;//This is the variable declaration.
+	int b=8;//This is the variable declaration.
+	
+	public void add() {//This is the method declaration
+		
+		int c=a+b;
+	}
+
+}
