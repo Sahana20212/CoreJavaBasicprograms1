4019c6fb4e49afe16114a794b6ae88f00dd7e270
Sahana20212 <96504836+Sahana20212@users.noreply.github.com>
2023-05-22 07:05:15 -0500

    Swaping

---
+++ src/com/core/Swapnum.java
@@ -0,0 +1,18 @@
+package com.core;
+
+public class Swapnum {
+	
+	public static void main (String args[]) {
+		
+		int a =50;
+		int b=25;
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
